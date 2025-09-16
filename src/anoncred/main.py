a7badcb2a7fbdab9521bc4203fb284eed95b7230
Luis Díaz <ldiazn98@gmail.com>
2025-09-16 16:15:33 +0200

    Add submit function

--- src/anoncred/main.py
+++ src/anoncred/main.py
@@ -3,13 +3,15 @@ from fastapi import HTTPException
 from sqlmodel import select
 from fastapi import FastAPI, Response
 from .utils import to_bin, to_str
-from .models import create_db_and_tables, init_server_state, ServerState
+from .models import create_db_and_tables, init_server_state
+from . import models
 from pydantic import BaseModel
 from .dependencies import SessionDep, ServerStateDep
 import base64
 import logging
 
 logger = logging.getLogger(__file__)
+
 
 # -- < Life Cycle >  --------------------------------
 @asynccontextmanager
@@ -18,24 +20,25 @@ async def lifespan(app: FastAPI):
     init_server_state()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 
-# -- < API > ---------------------------------------- 
+# -- < API > ----------------------------------------
 @app.get("/")
 def read_root():
     return {"Welcome": "Too ooni anonymous credentials protocol"}
 
 
 class Measurement(BaseModel):
-    test_name : str
-    probe_cc : str
-    probe_asn : str
+    test_name: str
+    probe_cc: str
+    probe_asn: str
     # ... more data
 
 # -- < Manifest > ------------------------------------
 @app.get("/manifest")
-def manifest(server_state : ServerStateDep):
+def manifest(server_state: ServerStateDep):
     # This is the `access_policy_manifest`
     public_params = server_state.get_public_parameters()
     public_params_s = base64.b64encode(public_params).decode()
@@ -64,41 +67,70 @@ class RegisterResponse(BaseModel):
 
 
 @app.post("/register", response_model=RegisterResponse)
-def register(request: RegisterRequest, session: SessionDep, state : ServerStateDep):
+def register(request: RegisterRequest, session: SessionDep, state: ServerStateDep):
     req_s = request.credential_sign_request
     req = to_bin(req_s)
     result = state.handle_registration_request(req)
-    return RegisterResponse(credential_sign_response=to_str(result), emission_date=state.today())
+    return RegisterResponse(
+        credential_sign_response=to_str(result), emission_date=state.today()
+    )
+
 
 # -- < Submission > ----------------------------------
 class SubmitRequest(BaseModel):
     measurement: Measurement
     submit_request: str
     nym: str
-    age_range: list[int] # [10, 30]
-    measurement_count_range: list[int] # [100, 200]
+    age_range: list[int]  # [10, 30]
+    measurement_count_range: list[int]  # [100, 200]
+
 
 class SubmitResponse(BaseModel):
     submit_response: str
 
+
 @app.post("/submit", response_model=SubmitResponse)
-def submit(request: SubmitRequest, session : SessionDep, state : ServerStateDep):
+def submit(request: SubmitRequest, session: SessionDep, state: ServerStateDep):
     request_bin = to_bin(request.submit_request)
     nym_bin = to_bin(request.nym)
 
     result = state.handle_submit_request(
-        nym_bin, 
-        request_bin, 
-        request.measurement.probe_cc, 
-        request.measurement.probe_asn, 
-        request.age_range, 
-        request.measurement_count_range
+        nym_bin,
+        request_bin,
+        request.measurement.probe_cc,
+        request.measurement.probe_asn,
+        request.age_range,
+        request.measurement_count_range,
     )
 
+    session.add(
+        models.Measurement(
+            nym=nym_bin,
+            probe_cc=request.measurement.probe_cc,
+            probe_asn=request.measurement.probe_asn,
+            test_name=request.measurement.test_name,
+        )
+    )
+    session.commit()
+
     return SubmitResponse(submit_response=to_str(result))
+
+class MeasurementsResponse(BaseModel):
+    results : list[Measurement]
+
+@app.get("/measurements", response_model=MeasurementsResponse)
+def get_measurements(session : SessionDep):
+    q = select(models.Measurement).limit(100)
+    results = session.scalars(q)
+    response = MeasurementsResponse(results=[])
+    for result in results:
+        response.results.append(
+            Measurement(test_name=result.test_name, probe_cc=result.probe_cc, probe_asn=result.probe_asn)
+        )
+    
+    return response
 
 # -- < Credential update > ---------------------------
 @app.post("/update/credentials")
 def update_credentials():
     return {"update_credentials"}
-
--- tests/test_creds.py
+++ tests/test_creds.py
@@ -29,22 +29,36 @@ def test_workflow(client):
     user.handle_registration_response(response_bin)
 
     # Submission
-    submit_req = user.make_submit_request(probe_asn="AS1234", probe_cc="VE", emission_date=emission_date)
+    response = submit_request(user, client, "AS1234", "VE", emission_date)
+    user.handle_submit_response(response)
 
+    # try more submissions
+    response = submit_request(user, client, "AS4321", "ES", emission_date)
+    user.handle_submit_response(response)
+
+    response = submit_request(user, client, "AS9999", "IT", emission_date)
+    user.handle_submit_response(response)
+
+    # Check that measurements where saved to DB
+    resp = getj(client, "/measurements")
+    assert len(resp['results']) == 3
+
+
+def submit_request(user : ooni.UserState, client : Client, probe_asn : str, probe_cc : str, emission_date : int) -> bytes:
+    submit_req = user.make_submit_request(probe_asn=probe_asn, probe_cc=probe_cc, emission_date=emission_date)
     submit_resp = postj(client, "/submit", json={
         "nym" : to_str(submit_req.nym),
         "submit_request" : to_str(submit_req.request),
         "measurement" : {
-            "probe_cc" : "VE",
-            "probe_asn" : "AS1234",
+            "probe_cc" : probe_cc,
+            "probe_asn" : probe_asn,
             "test_name" : "web_connectivity"
         },
         "age_range" : [emission_date - 30, emission_date + 1],
         "measurement_count_range" : [0, 100]
     })
 
-    submit_bin = to_bin(submit_resp['submit_response'])
-    user.handle_submit_response(submit_bin)
+    return to_bin(submit_resp['submit_response'])
 
 def getj(client: Client, path : str):
     resp = client.get(path)
